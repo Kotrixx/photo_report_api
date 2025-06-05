8047034b139feb14130a6f3f2093a4aed89c86b0
Kotrixx <a20206331@pucp.edu.pe>
2025-06-05 01:05:35 -0500

    Revert "cambios de autorizacion 1.13"
    This reverts commit fcad19de02162e58a9284de788ab37151324f390.

--- app/api_app.py
+++ app/api_app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Depends
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
 from starlette.middleware import Middleware
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.responses import RedirectResponse, JSONResponse
 
