92915e76503d7b64e395d3cceabb7df8d1c111d7
Kotrixx <a20206331@pucp.edu.pe>
2025-06-16 16:51:52 -0500

    last functional v2.1

--- app/routes/security_api.py
+++ app/routes/security_api.py
@@ -67,7 +67,7 @@ async def login(login_data: LoginData):
         print(f"Error inesperado: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error interno del servidor, por favor intente más tarde."
+            detail=f"Error interno del servidor, por favor intente más tarde. {e}"
         )
 
 
